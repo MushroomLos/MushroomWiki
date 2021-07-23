069960426f1d972def675d2e915bca8545b08842
MushroomLos <mushroomlos@outlook.com>
2021-07-23 22:17:25 +0800

    wiki 1.1.3 集成mybatis generator，生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -8,3 +8,13 @@ create  table `test`
 ) engine=innodb default charset=utf8mb4 comment='test';
 
 insert into `test` (id, name, password) values(1, 'test', 'password');
+
+drop table if exists `demo`;
+create  table `demo`
+(
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment 'name',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='test';
+
+insert into `demo` (id, name) values(1, 'test');
