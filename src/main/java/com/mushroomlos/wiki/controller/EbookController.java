cfe47950d1ffc8122a783a1b824446912d658038
MushroomLos <mushroomlos@outlook.com>
2021-07-23 22:43:48 +0800

    wiki 1.1.5 通用返回雷commonresp，之后的返回类型都以此为准。

---
+++ src/main/java/com/mushroomlos/wiki/controller/EbookController.java
@@ -0,0 +1,29 @@
+package com.mushroomlos.wiki.controller;
+
+
+import com.mushroomlos.wiki.domain.Ebook;
+import com.mushroomlos.wiki.resp.CommonResp;
+import com.mushroomlos.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list =  ebookService.list();
+        resp.setContent(list);
+        return resp;
+    }
+}
