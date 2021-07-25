ea9dd1a62463a7b7dc5f4cf4037bac5fe73be822
MushroomLos <mushroomlos@outlook.com>
2021-07-25 23:19:36 +0800

    wiki 1.2.1 wiki首页路由开发

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
+    <a-layout-header class="header" id="components-layout-demo-top-side">
       <div class="logo" />
       <a-menu
               theme="dark"
@@ -13,58 +13,7 @@
         <a-menu-item key="3">nav 3</a-menu-item>
       </a-menu>
     </a-layout-header>
-    <a-layout>
-      <a-layout-sider width="200" style="background: #fff">
-        <a-menu
-                mode="inline"
-                v-model:selectedKeys="selectedKeys2"
-                v-model:openKeys="openKeys"
-                :style="{ height: '100%', borderRight: 0 }"
-        >
-          <a-sub-menu key="sub1">
-            <template #title>
-              <span>
-                <user-outlined />
-                subnav 1
-              </span>
-            </template>
-            <a-menu-item key="1">option1</a-menu-item>
-            <a-menu-item key="2">option2</a-menu-item>
-            <a-menu-item key="3">option3</a-menu-item>
-            <a-menu-item key="4">option4</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
-            </template>
-            <a-menu-item key="5">option5</a-menu-item>
-            <a-menu-item key="6">option6</a-menu-item>
-            <a-menu-item key="7">option7</a-menu-item>
-            <a-menu-item key="8">option8</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub3">
-            <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
-            </template>
-            <a-menu-item key="9">option9</a-menu-item>
-            <a-menu-item key="10">option10</a-menu-item>
-            <a-menu-item key="11">option11</a-menu-item>
-            <a-menu-item key="12">option12</a-menu-item>
-          </a-sub-menu>
-        </a-menu>
-      </a-layout-sider>
-      <a-layout-content
-              :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-      >
-        Content
-      </a-layout-content>
-    </a-layout>
+    <router-view/>
     <a-layout-footer style="text-align: center">
       MushroomLos Wiki
     </a-layout-footer>
@@ -72,7 +21,7 @@
 </template>
 
 <style>
-  #components-layout-demo-top-side-2 .logo {
+  #components-layout-demo-top-side .logo {
     float: left;
     width: 120px;
     height: 31px;
@@ -80,7 +29,7 @@
     background: rgba(255, 255, 255, 0.3);
   }
 
-  .ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  .ant-row-rtl #components-layout-demo-top-side .logo {
     float: right;
     margin: 16px 0 16px 24px;
   }
