9bac3f9531ff629ef2d438b1ee3fa83f1b09fb4e
MushroomLos <mushroomlos@outlook.com>
2021-07-25 23:28:02 +0800

    wiki 1.2.2 制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <a-layout-footer style="text-align: center">
+        MushroomLos Wiki
+    </a-layout-footer>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'the-footer',
+    });
+</script>
