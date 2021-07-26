dba836001cd9a27b6243bfb2dff96807f4b32beb
MushroomLos <mushroomlos@outlook.com>
2021-07-26 20:55:55 +0800

    wiki 1.3.5 支持动态sql，name参数有值按name查，无值查全部，封面小图样式调整。

--- src/main/java/com/mushroomlos/wiki/service/EbookService.java
+++ src/main/java/com/mushroomlos/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.mushroomlos.wiki.req.EbookReq;
 import com.mushroomlos.wiki.resp.EbookResp;
 import com.mushroomlos.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -21,7 +22,9 @@ public class EbookService {
         EbookExample ebookExample = new EbookExample();
         // Criteria类似于where条件
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%" + req.getName() + "%");
+        if(!ObjectUtils.isEmpty(req.getName())) {
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
 //        List<EbookResp> respList = new ArrayList<>();
