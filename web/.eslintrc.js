8b6ed45e3a7bfdfa0bbdc8dee7f65c91cbfa850f
MushroomLos <mushroomlos@outlook.com>
2021-07-26 20:43:36 +0800

    wiki 1.3.3 集成图标组件

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     "vue/no-parsing-error": [2, { "x-invalid-end-tag": false }],
-    'vue/no-unused-components': 'off'
+    'vue/no-unused-components': 'off',
+    '@typescript-eslint/no-explicit-any': 0
   }
 }
