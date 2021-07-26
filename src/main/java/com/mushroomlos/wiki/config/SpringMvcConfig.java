55b7abad4c91437fe8e8db7d2d44fedec0347d25
MushroomLos <mushroomlos@outlook.com>
2021-07-26 23:28:18 +0800

    wiki 1.3.12 增加拦截器，打印接口耗时

---
+++ src/main/java/com/mushroomlos/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,45 @@
+package com.mushroomlos.wiki.config;
+
+import com.mushroomlos.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+//    @Resource
+//    LoginInterceptor loginInterceptor;
+
+//    @Resource
+//    ActionInterceptor actionInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login");
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns(
+//                        "/test/**",
+//                        "/redis/**",
+//                        "/user/login",
+//                        "/category/all",
+//                        "/ebook/list",
+//                        "/doc/all/**",
+//                        "/doc/vote/**",
+//                        "/doc/find-content/**",
+//                        "/ebook-snapshot/**"
+//                );
+//
+//        registry.addInterceptor(actionInterceptor)
+//                .addPathPatterns(
+//                        "/*/save",
+//                        "/*/delete/**",
+//                        "/*/reset-password");
+    }
+}
