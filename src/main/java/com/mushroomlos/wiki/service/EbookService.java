bef913c9a5ca1a68963d2d24c640ba9840e4d8ca
MushroomLos <mushroomlos@outlook.com>
2021-07-27 22:11:07 +0800

    wiki 1.4.4 增加PageHelper实现后端分页。

--- src/main/java/com/mushroomlos/wiki/service/EbookService.java
+++ src/main/java/com/mushroomlos/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.mushroomlos.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.mushroomlos.wiki.domain.Ebook;
 import com.mushroomlos.wiki.domain.EbookExample;
 import com.mushroomlos.wiki.mapper.EbookMapper;
 import com.mushroomlos.wiki.req.EbookReq;
 import com.mushroomlos.wiki.resp.EbookResp;
 import com.mushroomlos.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -15,17 +19,25 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<EbookResp> list(EbookReq req){
+    public List<EbookResp> list(EbookReq req) {
+
         EbookExample ebookExample = new EbookExample();
         // Criteria类似于where条件
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        if(!ObjectUtils.isEmpty(req.getName())) {
+        if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1, 3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("Total line: {}", pageInfo.getTotal());
+        LOG.info("Total pages: {}", pageInfo.getPages());
 
 //        List<EbookResp> respList = new ArrayList<>();
 //        for(Ebook ebook : ebookList){
