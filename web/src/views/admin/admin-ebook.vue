4415c2d457af98138fa949a38de0907c352c39e2
MushroomLos <mushroomlos@outlook.com>
2021-07-28 22:44:59 +0800

    wiki 1.4.11 集成validation，为列表接口增加参数校验

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -62,6 +62,7 @@
 <script lang="ts">
   import {defineComponent, onMounted, ref} from 'vue';
   import axios from 'axios';
+  import {message} from 'ant-design-vue';
 
   export default defineComponent({
     name: 'AdminEbook',
@@ -69,7 +70,7 @@
       const ebooks = ref();
       const pagination = ref({
         current: 1,
-        pageSize: 4,
+        pageSize: 1000,
         total: 0
       });
       const loading = ref(false);
@@ -130,11 +131,16 @@
         }).then((response) =>{
           loading.value = false;
           const data = response.data;
-          ebooks.value = data.content.list;
+          if(data.success){
+            ebooks.value = data.content.list;
 
-          // 重置分页按钮
-          pagination.value.current = params.page;
-          pagination.value.total = data.content.total;
+            // 重置分页按钮
+            pagination.value.current = params.page;
+            pagination.value.total = data.content.total;
+          }else{
+            console.log(data.success);
+            message.error(data.message);
+          }
         });
       };
 
