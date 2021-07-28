1f7337a78fe969f6013c3b22510f2b9dcb055127
MushroomLos <mushroomlos@outlook.com>
2021-07-28 23:04:30 +0800

    wiki 1.4.14 编辑时复制对象，修改表单时不会影响到列表数据。

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -76,6 +76,7 @@
   import {defineComponent, onMounted, ref} from 'vue';
   import axios from 'axios';
   import {message} from 'ant-design-vue';
+  import { Tool } from '@/util/tool';
 
   export default defineComponent({
     name: 'AdminEbook',
@@ -199,7 +200,7 @@
        */
       const edit = (record: any) =>{
         modalVisible.value = true;
-        ebook.value = record;
+        ebook.value = Tool.copy(record);
       };
 
       /**
