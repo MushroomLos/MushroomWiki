3cb7a394d5cb98091c20a959b017b816555206d4
MushroomLos <mushroomlos@outlook.com>
2021-07-29 23:34:59 +0800

    wiki 1.6.3 增加页面参数传递，完成新增文档功能。

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -75,10 +75,12 @@
   import axios from 'axios';
   import {message} from 'ant-design-vue';
   import { Tool } from '@/util/tool';
+  import {useRoute} from "vue-router";
 
   export default defineComponent({
     name: 'AdminDoc',
     setup(){
+      const route = useRoute();
       const param = ref();
       param.value = {};
       const docs = ref();
@@ -212,7 +214,9 @@
        */
       const add = () =>{
         modalVisible.value = true;
-        doc.value = {};
+        doc.value = {
+          ebookId: route.query.ebookId,
+        };
 
         treeSelectData.value = Tool.copy(level1.value);
         // 为选择树添加一个"无"
