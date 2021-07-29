37771a858e2db9fd8b2f67f332fa13d1dccd3201
MushroomLos <mushroomlos@outlook.com>
2021-07-29 15:54:59 +0800

    wiki 1.5.7 首页显示分类菜单。

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -7,41 +7,19 @@
               v-model:openKeys="openKeys"
               :style="{ height: '100%', borderRight: 0 }"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-                <span>
-                  <user-outlined />
-                  subnav 1
-                </span>
+        <a-menu-item key="welcome">
+          <router-link :to="'/'">
+            <MailOutlined />
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
+            <span><user-outlined />{{item.name}}</span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-                <span>
-                  <laptop-outlined />
-                  subnav 2
-                </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-                <span>
-                  <notification-outlined />
-                  subnav 3
-                </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined/><span>{{child.name}}</span>
+          </a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
@@ -74,6 +52,8 @@
 <script lang="ts">
 import { defineComponent, onMounted, ref, reactive} from 'vue';
 import axios from 'axios';
+import {message} from "ant-design-vue";
+import {Tool} from "@/util/tool";
 
 // const listData: any = [];
 // for (let i = 0; i < 23; i++) {
@@ -92,13 +72,37 @@ export default defineComponent({
   name: 'Home',
   setup(){
     const ebooks = ref();
-    const ebooks1 = reactive({books: []});
+    // const ebooks1 = reactive({books: []});
+
+    const level1 = ref();
+    let categorys: any;
+
+    const handleQueryCategory = () => {
+      axios.get("/category/all",).then((response) =>{
+        const data = response.data;
+        if(data.success){
+          categorys = data.content
+          console.log("原始数据:", categorys);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树型结构:", level1);
+        }else{
+          message.error(data.message);
+        }
+      });
+    };
+
+    const handleClick = () =>{
+      console.log("menu click")
+    }
 
     onMounted((()=> {
+      handleQueryCategory();
       axios.get("/ebook/list", {
         params: {
           page: 1,
-          size: 10,
+          size: 1000,
         }
       }).then((response) =>{
         const data = response.data;
@@ -122,6 +126,9 @@ export default defineComponent({
         { type: 'LikeOutlined', text: '156' },
         { type: 'MessageOutlined', text: '2' },
       ],
+
+      handleClick,
+      level1,
     }
   }
 });
