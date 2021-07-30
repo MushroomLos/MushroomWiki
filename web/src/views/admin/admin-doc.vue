f9a2472399a2c2c88598d6226487639e5ccb6466
MushroomLos <mushroomlos@outlook.com>
2021-07-30 11:17:29 +0800

    wiki 1.6.4 增加删除功能，能删除整个分支，并且提供二次确认。

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -32,9 +32,16 @@
             <a-button type="primary" @click="edit(record)">
               编辑
             </a-button>
-            <a-button type="danger" @click="handleDelete(record.id)">
-              删除
-            </a-button>
+            <a-popconfirm
+                    title="删除后不可恢复，确认删除?"
+                    ok-text="是"
+                    cancel-text="否"
+                    @confirm="handleDelete(record.id)"
+            >
+              <a-button type="danger">
+                删除
+              </a-button>
+            </a-popconfirm>
           </a-space>
         </template>
       </a-table>
@@ -71,11 +78,12 @@
 </template>
 
 <script lang="ts">
-  import {defineComponent, onMounted, ref} from 'vue';
+  import {defineComponent, onMounted, ref, createVNode} from 'vue';
   import axios from 'axios';
-  import {message} from 'ant-design-vue';
+  import {message, Modal} from 'ant-design-vue';
   import { Tool } from '@/util/tool';
   import {useRoute} from "vue-router";
+  import ExclamationCircleOutlined from "@ant-design/icons-vue/ExclamationCircleOutlined";
 
   export default defineComponent({
     name: 'AdminDoc',
@@ -194,6 +202,41 @@
         }
       }
 
+      const deleteIds: Array<string> = [];
+      const deleteNames: Array<string> = [];
+      /**
+       * 获取删除列表，查找整个树枝
+       */
+      const getDeleteIds = (treeSelectData: any, id: any) =>{
+        // console.log(treeSelectData, id);
+        // 遍历数组
+        for(let i = 0; i < treeSelectData.length; i++){
+          const node = treeSelectData[i];
+          if(node.id == id){
+            // 如果当前为目标
+            console.log("delete", node);
+            // 将目标ID放入结果集ids
+            // node.disabled = true;
+            deleteIds.push(id);
+            deleteNames.push(node.name);
+
+            // 遍历所有子节点
+            const children = node.children;
+            if(Tool.isNotEmpty(children)){
+              for(let j = 0; j < children.length; j++){
+                getDeleteIds(children, children[j].id);
+              }
+            }else{
+              // 如果当前节点不是子节点，去其他节点
+              const children = node.children;
+              if(Tool.isNotEmpty(children)) {
+                setDisable(children, id);
+              }
+            }
+          }
+        }
+      }
+
       /**
        * 编辑
        */
@@ -207,7 +250,7 @@
 
         // 为选择树添加一个“无”
         treeSelectData.value.unshift({id: 0, name: '无'});
-      };
+      }
 
       /**
        * 新增
@@ -226,15 +269,24 @@
       /**
        * 删除
        */
-      const handleDelete = (id: number) =>{
-        axios.delete("/doc/delete/" + id).then((response) =>{
-          const data = response.data; // data = commonResp
-          if(data.success){
-            // reloading
-            handleQuery();
-          }
+      const handleDelete = (id: number) => {
+        getDeleteIds(level1.value, id);
+        Modal.confirm({
+          title: '重要提醒',
+          icon: createVNode(ExclamationCircleOutlined),
+          content: '将删除：【' + deleteNames.join("，") + "】删除后不可恢复，确认删除？",
+          onOk() {
+            // console.log(ids)
+            axios.delete("/doc/delete/" + deleteIds.join(",")).then((response) => {
+              const data = response.data; // data = commonResp
+              if (data.success) {
+                // 重新加载列表
+                handleQuery();
+              }
+            });
+          },
         });
-      };
+      }
 
       onMounted(() => {
         handleQuery();
