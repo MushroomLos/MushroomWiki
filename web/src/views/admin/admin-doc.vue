ebad3911f021672e8665bd7c15c91fa14642c9d3
MushroomLos <mushroomlos@outlook.com>
2021-07-30 12:44:48 +0800

    wiki 1.6.8 文档管理布局优化，表格紧凑，按钮变小，表单变大，页面打开时初始化文本框，zindex变小。

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -3,7 +3,7 @@
     <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-row>
+      <a-row :gutter="24">
         <a-col :span="8">
           <p>
             <a-form layout="inline" :model="param">
@@ -25,13 +25,14 @@
                   :data-source="level1"
                   :loading="loading"
                   :pagination="false"
+                  size="small"
           >
-            <template #cover="{text:cover}">
-              <img v-if="cover" :src="cover" alt="avatar" />
+            <template #name="{ text, record }">
+              {{record.sort}} {{text}}
             </template>
             <template v-slot:action="{ text, record }">
               <a-space size="small">
-                <a-button type="primary" @click="edit(record)">
+                <a-button type="primary" @click="edit(record)" size="small">
                   编辑
                 </a-button>
                 <a-popconfirm
@@ -40,7 +41,7 @@
                         cancel-text="否"
                         @confirm="handleDelete(record.id)"
                 >
-                  <a-button type="danger">
+                  <a-button type="danger" size="small">
                     删除
                   </a-button>
                 </a-popconfirm>
@@ -49,9 +50,18 @@
           </a-table>
         </a-col>
         <a-col :span="16">
-          <a-form :model="doc" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
-            <a-form-item label="名称">
-              <a-input v-model:value="doc.name"/>
+          <p>
+            <a-form layout="inline" :model="param">
+              <a-form-item>
+                <a-button type="primary" @click="handleSave()">
+                  保存
+                </a-button>
+              </a-form-item>
+            </a-form>
+          </p>
+          <a-form :model="doc" layout="vertical">
+            <a-form-item>
+              <a-input v-model:value="doc.name" placeholder="名称"/>
             </a-form-item>
             <a-form-item label="父文档">
               <a-tree-select
@@ -65,10 +75,10 @@
               >
               </a-tree-select>
             </a-form-item>
-            <a-form-item label="顺序">
-              <a-input v-model:value="doc.sort"/>
+            <a-form-item>
+              <a-input v-model:value="doc.sort" placeholder="顺序"/>
             </a-form-item>
-            <a-form-item label="内容">
+            <a-form-item>
               <div id="content"></div>
             </a-form-item>
           </a-form>
@@ -107,16 +117,8 @@
       const columns = [
         {
           title: '名称',
-          dataIndex: 'name'
-        },
-        {
-          title: '父文档',
-          key: 'parent',
-          dataIndex: 'parent'
-        },
-        {
-          title: '顺序',
-          dataIndex: 'sort'
+          dataIndex: 'name',
+          slots: {customRender: 'name'}
         },
         {
           title: 'Action',
@@ -166,8 +168,9 @@
       const modalVisible = ref(false);
       const modalLoading = ref(false);
       const editor = new E('#content');
+      editor.config.zIndex = 0;
 
-      const handleModalOk = () =>{
+      const handleSave = () =>{
         modalLoading.value = true;
         axios.post("/doc/save", doc.value).then((response) =>{
           modalLoading.value = false;
@@ -262,9 +265,6 @@
 
         // 为选择树添加一个"无"
         treeSelectData.value.unshift({id: 0, name: '无'});
-        setTimeout(function () {
-          editor.create();
-        }, 100);
       };
 
       /**
@@ -280,9 +280,6 @@
 
         // 为选择树添加一个"无"
         treeSelectData.value.unshift({id: 0, name: '无'});
-        setTimeout(function () {
-          editor.create();
-        }, 100);
       };
       /**
        * 删除
@@ -312,6 +309,8 @@
 
       onMounted(() => {
         handleQuery();
+
+        editor.create();
       });
 
       return{
@@ -330,7 +329,7 @@
         doc,
         modalVisible,
         modalLoading,
-        handleModalOk
+        handleSave,
       }
     }
   });
