0ffc475c611d3d1e889108f7dea946cf673332f1
MushroomLos <mushroomlos@outlook.com>
2021-07-31 11:35:32 +0800

    wiki 1.6.19 增加提前预览。

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -81,11 +81,24 @@
               <a-input v-model:value="doc.sort" placeholder="顺序"/>
             </a-form-item>
             <a-form-item>
+                <a-form-item>
+                    <a-button type="primary" @click="handlePreviewContent()">
+                        <EyeOutlined/>
+                        内容预览
+                    </a-button>
+                </a-form-item>
+            </a-form-item>
+            <a-form-item>
               <div id="content"></div>
             </a-form-item>
           </a-form>
         </a-col>
       </a-row>
+
+       <a-drawer width="900" placement="right" :closable="false" :visible="drawerVisible" @close="onDrawerClose">
+         <div class="wangeditor" :innerHTML="previewHtml"></div>
+       </a-drawer>
+
     </a-layout-content>
   </a-layout>
 
@@ -339,6 +352,18 @@
         });
       };
 
+        // ----------------富文本预览--------------
+        const drawerVisible = ref(false);
+        const previewHtml = ref();
+        const handlePreviewContent = () => {
+            const html = editor.txt.html();
+            previewHtml.value = html;
+            drawerVisible.value = true;
+        };
+        const onDrawerClose = () => {
+            drawerVisible.value = false;
+        };
+
       onMounted(() => {
         handleQuery();
 
@@ -357,6 +382,10 @@
         add,
         handleDelete,
         treeSelectData,
+        drawerVisible,
+        previewHtml,
+        handlePreviewContent,
+        onDrawerClose,
 
         doc,
         modalVisible,
