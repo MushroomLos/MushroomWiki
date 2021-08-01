8e0bc1e3c9bb5a942756def84d1921d5fdcb93bb
MushroomLos <mushroomlos@outlook.com>
2021-08-01 11:48:53 +0800

    wiki 1.7.4 密码加密传输和加密存储。

--- src/main/java/com/mushroomlos/wiki/controller/UserController.java
+++ src/main/java/com/mushroomlos/wiki/controller/UserController.java
@@ -7,6 +7,7 @@ import com.mushroomlos.wiki.resp.CommonResp;
 import com.mushroomlos.wiki.resp.PageResp;
 import com.mushroomlos.wiki.resp.UserQueryResp;
 import com.mushroomlos.wiki.service.UserService;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -30,6 +31,7 @@ public class UserController {
 
     @PostMapping("/save")
     public CommonResp save(@Valid @RequestBody UserSaveReq req){
+        req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp resp = new CommonResp<>();
         userService.save(req);
         return resp;
