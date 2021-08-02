0a629a5d58f825590f5de35e4ebc4998738f2608
MushroomLos <mushroomlos@outlook.com>
2021-08-02 11:30:52 +0800

    wiki 1.7.12 使用sessionStorage存储用户信息，使其能够在刷新时被保留下来。

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -48,7 +48,7 @@
 </template>
 
 <script lang="ts">
-    import { defineComponent, ref } from 'vue';
+    import {computed, defineComponent, ref} from 'vue';
     import axios from 'axios';
     import {message} from 'ant-design-vue'
     import store from '@/store';
@@ -60,8 +60,8 @@
         name: 'the-header',
         setup () {
             // 登录后保存
-            const user = ref();
-            user.value = {};
+            const user = computed(() => store.state.user);
+
             //登录
             const loginUser = ref({
                 loginName: "test1",
@@ -84,8 +84,7 @@
                     if (data.success) {
                         loginModalVisible.value = false;
                         message.success("登录成功！");
-                        user.value = data.content;
-                        store.commit("setUser", user.value);
+                        store.commit("setUser", data.content);
                     } else {
                         message.error(data.message);
                     }
