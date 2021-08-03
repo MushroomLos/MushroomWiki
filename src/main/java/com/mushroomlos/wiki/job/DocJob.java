e154f7ae73dd3259836f97a183d1ec8043ac3441
MushroomLos <mushroomlos@outlook.com>
2021-08-03 16:48:59 +0800

    wiki 1.8.5 增加日志流水号。

--- src/main/java/com/mushroomlos/wiki/job/DocJob.java
+++ src/main/java/com/mushroomlos/wiki/job/DocJob.java
@@ -4,6 +4,7 @@ import com.mushroomlos.wiki.service.DocService;
 import com.mushroomlos.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -26,7 +27,7 @@ public class DocJob {
     @Scheduled(cron = "5/30 * * * * ?")
     public void cron() {
         // 增加日志流水号
-//        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
         LOG.info("更新电子书下的文档数据开始");
         long start = System.currentTimeMillis();
         docService.updateEbookInfo();
