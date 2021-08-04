7e3d4f3dab7a2788ee26fa471233861e64c3ed90
MushroomLos <mushroomlos@outlook.com>
2021-08-04 11:11:54 +0800

    wiki 1.8.7 完成点赞通知功能，点赞时，组装消息内容，往WS推送，前端收到WS消息后，弹出消息内存。。

--- src/main/java/com/mushroomlos/wiki/service/DocService.java
+++ src/main/java/com/mushroomlos/wiki/service/DocService.java
@@ -18,6 +18,7 @@ import com.mushroomlos.wiki.util.CopyUtil;
 import com.mushroomlos.wiki.util.RedisUtil;
 import com.mushroomlos.wiki.util.RequestContext;
 import com.mushroomlos.wiki.util.SnowFlake;
+import com.mushroomlos.wiki.websocket.WebSocketServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -45,6 +46,9 @@ public class DocService {
 
     @Resource
     public RedisUtil redisUtil;
+
+    @Resource
+    public WebSocketServer webSocketServer;
 
     public List<DocQueryResp> all(Long ebookId) {
 
@@ -147,6 +151,10 @@ public class DocService {
         }else{
             throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
         }
+
+        // 推送消息
+        Doc docDb = docMapper.selectByPrimaryKey(id);
+        webSocketServer.sendInfo("【" + docDb.getName() + "】被点赞");
     }
 
     public void updateEbookInfo(){
