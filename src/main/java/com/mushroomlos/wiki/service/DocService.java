89abe811557a4be1b790eb00922271c440dd4a07
MushroomLos <mushroomlos@outlook.com>
2021-08-04 11:58:18 +0800

    wiki 1.8.9 将业务线程日志流水号传递到异步线程，方便生产运维检查问题。

--- src/main/java/com/mushroomlos/wiki/service/DocService.java
+++ src/main/java/com/mushroomlos/wiki/service/DocService.java
@@ -20,6 +20,7 @@ import com.mushroomlos.wiki.util.RequestContext;
 import com.mushroomlos.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -153,7 +154,8 @@ public class DocService {
 
         // 推送消息
         Doc docDb = docMapper.selectByPrimaryKey(id);
-        wsService.sendInfo("【" + docDb.getName() + "】被点赞");
+        String logId = MDC.get("LOG_ID");
+        wsService.sendInfo("【" + docDb.getName() + "】被点赞", logId);
     }
 
     public void updateEbookInfo(){
