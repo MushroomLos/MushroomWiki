2bd4baa9f9c40ec33bd0ff86038cf2bc997fd01a
MushroomLos <mushroomlos@outlook.com>
2021-08-05 11:36:00 +0800

    wiki 1.9.6 集成echars，使用对应示例。

--- web/src/components/the-welcome.vue
+++ web/src/components/the-welcome.vue
@@ -87,12 +87,21 @@
                 </a-card>
             </a-col>
         </a-row>
+        <a-row>
+            <a-col :span="24">
+                <div id="main" style="width: 100%; height:300px;">
+
+                </div>
+            </a-col>
+        </a-row>
     </div>
 </template>
 
 <script lang="ts">
     import {defineComponent, ref, onMounted} from 'vue';
     import axios from 'axios'
+
+    declare let echarts: any;
 
     export default defineComponent({
         name: 'the-welcome',
@@ -122,8 +131,54 @@
                 });
             };
 
+            const testECharts = () => {
+                const chartDom = document.getElementById('main');
+                const myChart = echarts.init(chartDom);
+                let option;
+
+                option = {
+                    tooltip: {
+                        trigger: 'axis',
+                        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                        }
+                    },
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '3%',
+                        containLabel: true
+                    },
+                    xAxis: [
+                        {
+                            type: 'category',
+                            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                            axisTick: {
+                                alignWithLabel: true
+                            }
+                        }
+                    ],
+                    yAxis: [
+                        {
+                            type: 'value'
+                        }
+                    ],
+                    series: [
+                        {
+                            name: '直接访问',
+                            type: 'bar',
+                            barWidth: '60%',
+                            data: [10, 52, 200, 334, 390, 330, 220]
+                        }
+                    ]
+                };
+
+                myChart.setOption(option);
+            }
+
             onMounted(() => {
                 getStatistic();
+                testECharts();
             });
 
             return {
