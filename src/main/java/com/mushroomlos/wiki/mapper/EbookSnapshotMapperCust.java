9d6629deddcb0571dd6fae4e218b58f28fbc0dbb
MushroomLos <mushroomlos@outlook.com>
2021-08-05 10:28:35 +0800

    wiki 1.9.3 后端增加获取统计数值接口。

--- src/main/java/com/mushroomlos/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/mushroomlos/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,6 +1,12 @@
 package com.mushroomlos.wiki.mapper;
 
+import com.mushroomlos.wiki.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
 
     public void genSnapshot();
+
+    List<StatisticResp> getStatistic();
 }
